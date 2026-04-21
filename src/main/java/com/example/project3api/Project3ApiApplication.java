9042e9cbcadda94bab4ee6969559cafb95f410ae
Affirmingfour61 <raiderjoshjc81@gmail.com>
2026-04-21 14:27:28 -0700

    Added SpringBoot

---
+++ src/main/java/com/example/project3api/Project3ApiApplication.java
@@ -0,0 +1,12 @@
+
+package com.example.project3api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Project3ApiApplication.class, args);
+    }
+}
